8d3a00de5d2f033b0717dab4ed09718e6dc92727
Andy Dobbs <andrew.dobbs@gmail.com>
2023-03-25 11:29:31 -0400

    Add design case study page and first block of content

---
+++ pages/design-case-study.tsx
@@ -0,0 +1,18 @@
+export default function DesignCaseStudy() {
+    return (
+        <>
+            <h1>AI/ML Preventive Health Conversations</h1>
+            <p>The overall business goal for the Virgin Pulse health and wellbeing platform is to drive population health outcomes for employers (buyers) by improving personal health among employees (users).</p>
+
+            <p>VP IQ is an iOS, Android, and web experience that delivers on this goal by helping people improve health outcomes through personalized interactions powered by artificial intelligence, behavioral science, and machine learning.</p>
+            
+            <p>The conversational experience adapts to signals of motivation and demotivation, then explores the underlying “why” behind the change before prompting action or delivering content that is ranked by an ML algorithm. This includes all users in an intelligent feedback loop that helps guide them to the next best healthy action.</p>
+            
+            <p>In addition to leading design on this feature, I led design of personalization and recommendation features for Virgin Pulse, a platform that reaches 14 million people and spans 22 languages. I created and socialized the design strategy for personalization that informs the work of a global 60 person product organization and 22 engineering teams.</p>
+            
+            <p>I also contributed UX research, content strategy, and UX writing work on this feature, and I brought in content and research specialists as early as I could to work side-by-side with design, product management, and engineering. Bringing everyone together helped our team maximize the quality of our vision and the excellence of our execution.</p>
+            
+            <p>Data gathered through UX research showed that personalization features like VP IQ that I designed are a key driver of behavior change and satisfaction. 87% of users said Virgin Pulse changed their lives, and our NPS score was above 50. VP IQ, the signature personalization feature in the platform, even received a virtual standing ovation from our client success team.</p>
+        </>
+    );
+}
