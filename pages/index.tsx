5298bb724760a03f5ad87704b3303738d1f16e6b
Andy Dobbs <andrew.dobbs@gmail.com>
2023-03-26 12:44:32 -0400

    Reformat import statements

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from "next/link";
-import { Inter } from 'next/font/google'
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] })
 
