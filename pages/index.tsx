5f87513109c41e8669c99c1689dd30b2f6eaf942
Andy Dobbs <andrew.dobbs@gmail.com>
2023-03-31 09:51:55 -0400

    Change homepage to link to case study and code sample

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,37 +17,32 @@ export default function Home() {
       <div className="container mx-auto px-4 py-8">
           <nav className="flex flex-row justify-center mb-20">
             <a className="text-zinc-300 hover:text-[#aaff80] underline underline-offset-2 text-base font-light mr-8" 
-                href="/AndyDobbsResume.pdf" target="_blank">Resume</a>
+              href="/AndyDobbsResume.pdf" target="_blank">Resume</a>
             <a className="text-zinc-300 hover:text-[#aaff80] underline underline-offset-2 text-base font-light mr-8" 
-                href="https://www.linkedin.com/in/dobbs/" target="_blank">LinkedIn</a>
+              href="https://www.linkedin.com/in/dobbs/" target="_blank">LinkedIn</a>
             <a className="text-zinc-300 hover:text-[#aaff80] underline underline-offset-2 text-base font-light" 
-                href="https://github.com/adobbs" target="_blank">GitHub</a>
+              href="https://github.com/adobbs" target="_blank">GitHub</a>
           </nav>
           <div className="flex flex-col items-center">
             <Image
-                className="rounded-full border border-white mb-4"
-                src="/headshot.png"
-                alt="Andy Dobbs headshot"
-                width={80}
-                height={80}
-                priority
-                unoptimized
+              className="rounded-full border border-white mb-4"
+              src="/headshot.png"
+              alt="Andy Dobbs headshot"
+              width={80}
+              height={80}
+              priority
+              unoptimized
             />
             <h1 className="text-2xl md:text-4xl text-center mb-2">Andy Dobbs</h1>
             <h2 className="text-zinc-300 text-base md:text-xl text-center font-light mb-12">UX Engineer</h2>
-            <section className="bg-zinc-700 flex flex-col items-center text-base max-w-3xl mb-6 p-4 md:py-8 md:px-10 border rounded-lg">
-                <Image
-                    src="/vp-iq-steps.png"
-                    alt="VP IQ steps screenshots"
-                    width={960}
-                    height={540}
-                    priority
-                />
-                <h2 className="text-base md:text-xl text-center font-light mb-12">AI/ML Health Conversations</h2>
-                <Link href="/design-case-study">
-                  <button className="hover:text-[#aaff80] py-2 px-6 border hover:border-[#aaff80]  rounded-full">View case study</button>
-                </Link>
-            </section>
+            <div className="flex flex-row items-center">
+              <Link href="/design-case-study">
+                <button className="hover:text-[#aaff80] hover:border-[#aaff80] mr-4 py-2 px-6 border rounded-full">design case</button>
+              </Link>
+              <Link href="https://github.com/adobbs/portfolio" target="_blank">
+                <button className="hover:text-[#aaff80] hover:border-[#aaff80] py-2 px-6 border rounded-full">code sample</button>
+              </Link>
+            </div>
           </div>
       </div>
     </>
