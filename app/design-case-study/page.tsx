ea285826845d0ff393e96560947e42cf893de067
Andy Dobbs <andrew.dobbs@gmail.com>
2023-05-14 09:54:56 -0400

    Maximize quality of images

--- app/design-case-study/page.tsx
+++ app/design-case-study/page.tsx
@@ -27,12 +27,13 @@ export default function DesignCaseStudy() {
                         <h1 className="text-2xl md:text-4xl font-bold tracking-tight uppercase mb-4 md:mb-8">Design Case Study</h1>
                         <h2 className="text-xl md:text-2xl font-semibold tracking-tight uppercase mb-2 md:mb-4">Introduction</h2>
                         <Image
-                                className="mb-4"
-                                src="/vp-iq-steps.png"
-                                alt="VP IQ steps screenshots"
-                                width={1755}
-                                height={936}
-                                priority
+                            className="mb-4"
+                            src="/vp-iq-steps.png"
+                            alt="VP IQ steps screenshots"
+                            width={1755}
+                            height={936}
+                            priority
+                            unoptimized
                         />
                         <p className="text-zinc-400 text-base mb-2 max-w-prose">The overall business goal for the Virgin Pulse health and wellbeing platform is to drive population health outcomes for employers (buyers) by improving personal health among employees (users).</p>
                         <p className="text-zinc-400 text-base mb-2 max-w-prose">VP IQ is an iOS, Android, and web experience that delivers on this goal by helping people improve health outcomes through personalized interactions powered by artificial intelligence, behavioral science, and machine learning.</p>
@@ -41,12 +42,13 @@ export default function DesignCaseStudy() {
                         <p className="text-zinc-400 text-base mb-12 max-w-prose">I also contributed UX research, content strategy, and UX writing work on this feature, and I brought in content and research specialists as early as I could to work side-by-side with design, product management, and engineering. Bringing everyone together helped our team maximize the quality of our vision and the excellence of our execution.</p>
                         <h2 className="text-xl md:text-2xl font-semibold tracking-tight uppercase mb-2 md:mb-4">Problem</h2>
                         <Image
-                                className="mb-4"
-                                src="/vp-iq-personas.png"
-                                alt="VP IQ persona summaries"
-                                width={1574}
-                                height={906}
-                                priority
+                            className="mb-4"
+                            src="/vp-iq-personas.png"
+                            alt="VP IQ persona summaries"
+                            width={1574}
+                            height={906}
+                            priority
+                            unoptimized
                         />
                         <p className="text-zinc-400 text-base mb-2 max-w-prose">Our existing recommendations directed people in a coarse way using machine learning content recommendations. This directed action, but not toward specific solutions that could drive outcomes targeted toward an identified need. And these recommendations were delivered without any signals from people&apos;s health data.</p>
                         <p className="text-zinc-400 text-base mb-2 max-w-prose">We knew from foundational research that I synthesized into personas that people like Mateo need help taking specific actions. And people like Shannon want timely, relevant support to keep on track. Without a guided experience, Mateo would fall off track. And without recognition, Shannon would have less insight into whether she is really achieving her goals.</p>
@@ -59,6 +61,7 @@ export default function DesignCaseStudy() {
                             width={1600}
                             height={900}
                             priority
+                            unoptimized
                         />
                         <p className="text-zinc-400 text-base mb-2 max-w-prose">We focused on a central question to guide our ideation: How might we personalize the health journey in a way that improves outcomes?</p>
                         <p className="text-zinc-400 text-base mb-2 max-w-prose">Inspired by cross-functional exploration, we harmonized around a dynamic set of conversational experiences that are triggered through statistical tests of validated health data.</p>
@@ -67,14 +70,15 @@ export default function DesignCaseStudy() {
                         <p className="text-zinc-400 text-base mb-2 max-w-prose">Most of the content in this experience is dynamic, and all the copy is served from a custom CMS we built for our content team. The CMS also stores translations in 22 different languages.</p>
                         <p className="text-zinc-400 text-base mb-12 max-w-prose">A variety of UX research studies conducted throughout the lifecycle of product discovery and delivery informed the solution every step of the way, helping us refine our approach. The biggest takeaways from that research are that agency increases trust, conversations create comfort, and exploring the root cause is essential.</p>
                         <h2 className="text-xl md:text-2xl font-semibold tracking-tight uppercase mb-2 md:mb-4">Results</h2>
-                            <Image
-                                className="rounded-3xl mb-4"
-                                src="/vp-iq-principles.png"
-                                alt="VP IQ design principles"
-                                width={1600}
-                                height={900}
-                                priority
-                            />
+                        <Image
+                            className="rounded-3xl mb-4"
+                            src="/vp-iq-principles.png"
+                            alt="VP IQ design principles"
+                            width={1600}
+                            height={900}
+                            priority
+                            unoptimized
+                        />
                         <p className="text-zinc-400 text-base mb-2 max-w-prose">Personalization is a broad topic that extends beyond the idea of curated ML recommendations. To help convey what we learned and help other designers and product teams build off that knowledge, I created and shared research-validated design principles for personalization used by the entire product organization.</p>
                         <p className="text-zinc-400 text-base mb-2 max-w-prose">The design, technical, and strategic foundations of this feature were used by many teams to improve how the platform drives impact across the product experience. As detailed in the introduction above, I led design of personalization across the organization, doing hands-on work with one engineering team and consulting with many teams across a global market leader.</p>
                         <p className="text-zinc-400 text-base max-w-prose">Data gathered through UX research showed that personalization features like VP IQ that I designed are a key driver of behavior change and satisfaction. 87% of users said Virgin Pulse changed their lives, and our NPS score was above 50. VP IQ, the signature personalization feature in the platform, even received a virtual standing ovation from our client success and growth teams due to its ability to help them tell an innovation and engagement story that drives revenue and health outcomes.</p>
