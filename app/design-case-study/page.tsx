1fb56fb2003a9b43083ab10aaf4de2d06f9e35dd
Andy Dobbs <andrew.dobbs@gmail.com>
2023-08-27 15:59:38 -0400

    Update section layout for image and title

--- app/design-case-study/page.tsx
+++ app/design-case-study/page.tsx
@@ -55,18 +55,18 @@ export default function DesignCaseStudy() {
                 <h1 className="text-3xl sm:text-5xl md:text-6xl font-extrabold uppercase"><em className="italic text-[#ffe347]">Design</em> Case Study</h1>
             </div>
             <div className="mx-auto mt-32 px-4 max-w-[1536px]">
-                <div className='md:mx-16 lg:mx-32 mt-20 py-16 items-center border-2 border-white'>
-                    <div className="flex flex-col self-start mx-8 lg:mx-24 max-w-4xl">
+                <div className='md:mx-16 lg:mx-32 mt-20 py-16 px-8 lg:px-24 border-2 border-white'>
+                    <Image
+                        className="mb-8 sm:mb-12"
+                        src="/vp-iq-steps.png"
+                        alt="VP IQ steps screenshots"
+                        width={1755}
+                        height={936}
+                        priority
+                        unoptimized
+                    />
+                    <div className="flex flex-col max-w-4xl">
                         <h2 className="text-xl sm:text-2xl md:text-3xl font-semibold uppercase mb-4 sm:mb-6">Introduction</h2>
-                        <Image
-                            className="mb-4 sm:mb-6"
-                            src="/vp-iq-steps.png"
-                            alt="VP IQ steps screenshots"
-                            width={1755}
-                            height={936}
-                            priority
-                            unoptimized
-                        />
                         <p className="text-base sm:text-lg md:text-xl mb-2 sm:mb-3 md:mb-4 max-w-prose">The overall business goal for the Virgin Pulse health and wellbeing platform is to drive population health outcomes for employers (buyers) by improving personal health among employees (users).</p>
                         <p className="text-base sm:text-lg md:text-xl mb-2 sm:mb-3 md:mb-4 max-w-prose">VP IQ is an iOS, Android, and web experience that delivers on this goal by helping people improve health outcomes through personalized interactions powered by artificial intelligence, behavioral science, and machine learning.</p>
                         <p className="text-base sm:text-lg md:text-xl mb-2 sm:mb-3 md:mb-4 max-w-prose">The conversational experience adapts to signals of motivation and demotivation, then explores the underlying reason behind the change before prompting action or delivering content that is ranked by an ML algorithm. This includes all users in an intelligent feedback loop that helps guide them to the next best healthy action.</p>
@@ -74,35 +74,35 @@ export default function DesignCaseStudy() {
                         <p className="text-base sm:text-lg md:text-xl max-w-prose">I also contributed UX research, content strategy, and UX writing work on this feature, and I brought in content and research specialists as early as I could to work side-by-side with design, product management, and engineering. Bringing everyone together helped our team maximize the quality of our vision and the excellence of our execution.</p>
                     </div>
                 </div>
-                <div className='md:mx-16 lg:mx-32 mt-20 py-16 items-center border-2 border-white'>
-                    <div className="flex flex-col self-start mx-8 lg:mx-24 max-w-4xl">
+                <div className='md:mx-16 lg:mx-32 mt-20 py-16 px-8 lg:px-24 border-2 border-white'>
+                    <Image
+                        className="mb-8 sm:mb-12"
+                        src="/vp-iq-personas.png"
+                        alt="VP IQ persona summaries"
+                        width={1574}
+                        height={906}
+                        priority
+                        unoptimized
+                    />
+                    <div className="flex flex-col max-w-4xl">
                         <h2 className="text-xl sm:text-2xl md:text-3xl font-semibold uppercase mb-4 sm:mb-6">Problem</h2>
-                        <Image
-                            className="mb-4 sm:mb-6"
-                            src="/vp-iq-personas.png"
-                            alt="VP IQ persona summaries"
-                            width={1574}
-                            height={906}
-                            priority
-                            unoptimized
-                        />
                         <p className="text-base sm:text-lg md:text-xl mb-2 sm:mb-3 md:mb-4 max-w-prose">Our existing recommendations directed people in a coarse way using machine learning content recommendations. This directed action, but not toward specific solutions that could drive outcomes targeted toward an identified need. And these recommendations were delivered without any signals from people&apos;s health data.</p>
                         <p className="text-base sm:text-lg md:text-xl mb-2 sm:mb-3 md:mb-4 max-w-prose">We knew from foundational research that I synthesized into personas that people like Mateo need help taking specific actions. And people like Shannon want timely, relevant support to keep on track. Without a guided experience, Mateo would fall off track. And without recognition, Shannon would have less insight into whether she is really achieving her goals.</p>
                         <p className="text-base sm:text-lg md:text-xl max-w-prose">We could only drive health outcomes if we got people to change their behavior in the real world. That means we needed to react to data signals from real world behaviors, and align our massive content library with the underlying needs driving changes in people&apos;s behavior. Unlike other recommendation systems, we didn&apos;t win by getting people to watch one more video, listen to one more song, add one more item to their cart, or keep scrolling their feed for one more hour. We won every time one more life was changed for the better.</p>
                     </div>
                 </div>
-                <div className='md:mx-16 lg:mx-32 mt-20 py-16 items-center border-2 border-white'>
-                    <div className="flex flex-col self-start mx-8 lg:mx-24 max-w-4xl">
-                        <h2 className="text-xl sm:text-2xl md:text-3xl font-semibold uppercase mb-4 sm:mb-6">Solution</h2>
-                        <Image
-                            className="rounded-3xl mb-4 sm:mb-6"
-                            src="/vp-iq-system.png"
+                <div className='md:mx-16 lg:mx-32 mt-20 py-16 px-8 lg:px-24 border-2 border-white'>
+                    <Image
+                        className="rounded-3xl mb-8 sm:mb-12"
+                        src="/vp-iq-system.png"
                             alt="VP IQ system diagram"
                             width={1600}
                             height={900}
                             priority
                             unoptimized
-                        />
+                    /> 
+                   <div className="flex flex-col max-w-4xl">
+                        <h2 className="text-xl sm:text-2xl md:text-3xl font-semibold uppercase mb-4 sm:mb-6">Solution</h2>
                         <p className="text-base sm:text-lg md:text-xl mb-2 sm:mb-3 md:mb-4 max-w-prose">Inspired by cross-functional exploration, we harmonized around a dynamic set of conversational experiences that are triggered through statistical tests of validated health data.</p>
                         <p className="text-base sm:text-lg md:text-xl mb-2 sm:mb-3 md:mb-4 max-w-prose">We focused on a central question to guide our ideation: How might we personalize the health journey in a way that improves outcomes?</p>
                         <p className="text-base sm:text-lg md:text-xl mb-2 sm:mb-3 md:mb-4 max-w-prose">Two of these experiences, Interventions and Reflections, lead to a choose-your-own-adventure interaction that curates subsets of recommended content based on the reason people provide for why their data has trended in a positive or negative direction.</p>
@@ -111,18 +111,18 @@ export default function DesignCaseStudy() {
                         <p className="text-base sm:text-lg md:text-xl max-w-prose">A variety of UX research studies conducted throughout the lifecycle of product discovery and delivery informed the solution every step of the way, helping us refine our approach. The biggest takeaways from that research are that agency increases trust, conversations create comfort, and exploring the root cause is essential.</p>
                     </div>
                 </div>
-                <div className='md:mx-16 lg:mx-32 mt-20 py-16 items-center border-2 border-white'>
-                    <div className="flex flex-col self-start mx-8 lg:mx-24 max-w-4xl">
-                        <h2 className="text-xl sm:text-2xl md:text-3xl font-semibold uppercase mb-4 sm:mb-6">Results</h2>
-                        <Image
-                            className="rounded-3xl mb-4 sm:mb-6"
-                            src="/vp-iq-principles.png"
+                <div className='md:mx-16 lg:mx-32 mt-20 py-16 px-8 lg:px-24 border-2 border-white'>
+                    <Image
+                        className="rounded-3xl mb-8 sm:mb-12"
+                        src="/vp-iq-principles.png"
                             alt="VP IQ design principles"
                             width={1600}
                             height={900}
                             priority
                             unoptimized
-                        />
+                    />
+                    <div className="flex flex-col max-w-4xl">
+                        <h2 className="text-xl sm:text-2xl md:text-3xl font-semibold uppercase mb-4 sm:mb-6">Results</h2>
                         <p className="text-base sm:text-lg md:text-xl mb-2 sm:mb-3 md:mb-4 max-w-prose">Personalization is a broad topic that extends beyond the idea of curated ML recommendations. To help convey what we learned and help other designers and product teams build off that knowledge, I created and shared research-validated design principles for personalization used by the entire product organization.</p>
                         <p className="text-base sm:text-lg md:text-xl mb-2 sm:mb-3 md:mb-4 max-w-prose">The design, technical, and strategic foundations of this feature were used by many teams to improve how the platform drives impact across the product experience. As detailed in the introduction above, I led design of personalization across the organization, doing hands-on work with one engineering team and consulting with many teams across a global market leader.</p>
                         <p className="text-base sm:text-lg md:text-xl max-w-prose">Data gathered through UX research showed that personalization features like VP IQ that I designed are a key driver of behavior change and satisfaction. 87% of users said Virgin Pulse changed their lives, and our NPS score was above 50. VP IQ, the signature personalization feature in the platform, even received a virtual standing ovation from our client success and growth teams due to its ability to help them tell an innovation and engagement story that drives revenue and health outcomes.</p>
