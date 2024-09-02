45d090059ec760ab5ff3e541418bf7ccdc1652be
Andy Dobbs <andrew.dobbs@gmail.com>
2024-09-02 21:14:12 +0000

    Fix some layout issues on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -88,8 +88,8 @@ export default function Home() {
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon.ico" />
         </Head>
-        <div className="flex flex-col min-h-screen">
-            <div className="flex-grow">
+        <div className="flex flex-col">
+            <main className="flex flex-col">
                 <div className="flex gap-4 justify-center items-center lg:mt-16 p-4">
                     <div className="w-8 h-6 bg-white -skew-x-[13deg]"></div>
                     <div className="w-32 h-6 bg-white -skew-x-[13deg]"></div>
@@ -98,7 +98,7 @@ export default function Home() {
                     <div className="w-8 h-6 bg-[#ffe347] -skew-x-[13deg]"></div>
                     <div className="w-8 h-6 bg-white -skew-x-[13deg]"></div>
                 </div>
-                <div className="flex gap-4 justify-center items-center mt-16 lg:mt-24">
+                <div className="flex gap-4 justify-center items-center mt-12 lg:mt-24">
                     <h1 className="text-3xl lg:text-6xl font-extrabold uppercase">Andy</h1>
                     <div className="w-2 lg:w-4 h-8 lg:h-16 bg-[#ffe347] -skew-x-[13deg]"></div>
                     <h1 className="text-3xl lg:text-6xl font-extrabold uppercase">Dobbs</h1>
@@ -121,7 +121,7 @@ export default function Home() {
                             <div className="w-16 h-6 bg-white -skew-x-[13deg]"></div>
                         </div>
                     </div>
-                    <div className="flex flex-col gap-8 lg:gap-16 mt-8 lg:mt-16 text-sm sm:text-base lg:text-2xl">
+                    <div className="flex flex-col gap-12 lg:gap-16 lg:mt-16 text-sm sm:text-base lg:text-2xl">
                         <NewsletterSignup
                             title="The Fairweather Series"
                             description="The next must-read epic fantasy. Take flight with the"
@@ -136,28 +136,30 @@ export default function Home() {
                         />
                         </div>
                 </div>
-            </div>
-            <div className="flex w-full gap-4 justify-center items-center mx-auto mt-auto lg:mt-32 lg:mb-16 max-w-[1024px] p-4">
-                <div className="w-full h-6 bg-white -skew-x-[13deg]"></div>
-                <a href="https://www.linkedin.com/in/dobbs/" target="_blank" className="hover:text-[#ffe347] hover:cursor-pointer">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path>
-                    <path d="M8 11l0 5"></path>
-                    <path d="M8 8l0 .01"></path>
-                    <path d="M12 16l0 -5"></path>
-                    <path d="M16 16v-3a2 2 0 0 0 -4 0"></path>
-                    </svg>
-                </a>
-                <a href="https://github.com/adobbs" target="_blank" className="hover:text-[#ffe347] hover:cursor-pointer">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
-                    </svg>
-                </a>
-                <div className="w-8 h-6 bg-white -skew-x-[13deg]"></div>
-            </div>
-        </div>
+            </main>
+            <footer className="mt-12 lg:mt-32 mb-4 lg:mb-16">
+              <div className="flex w-full gap-4 justify-center items-center mx-auto max-w-[1024px] p-4">
+                  <div className="w-full h-6 bg-white -skew-x-[13deg]"></div>
+                  <a href="https://www.linkedin.com/in/dobbs/" target="_blank" className="hover:text-[#ffe347] hover:cursor-pointer">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                      <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path>
+                      <path d="M8 11l0 5"></path>
+                      <path d="M8 8l0 .01"></path>
+                      <path d="M12 16l0 -5"></path>
+                      <path d="M16 16v-3a2 2 0 0 0 -4 0"></path>
+                      </svg>
+                  </a>
+                  <a href="https://github.com/adobbs" target="_blank" className="hover:text-[#ffe347] hover:cursor-pointer">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                      <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
+                      </svg>
+                  </a>
+                  <div className="w-8 h-6 bg-white -skew-x-[13deg]"></div>
+              </div>
+            </footer>
+          </div>
     </>
   )
 }
