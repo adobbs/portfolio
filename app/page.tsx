4454b20ea153ade545eae3073e665a4914a4bce5
Andy Dobbs <andrew.dobbs@gmail.com>
2024-09-13 12:40:47 +0000

    Change to coming soon

--- app/page.tsx
+++ app/page.tsx
@@ -103,38 +103,8 @@ export default function Home() {
                     <div className="w-2 lg:w-4 h-8 lg:h-16 bg-[#ffe347] -skew-x-[13deg]"></div>
                     <h1 className="text-3xl lg:text-6xl font-extrabold uppercase">Dobbs</h1>
                 </div>
-                <div className="mx-auto mt-16 lg:mt-32 px-4 max-w-[1024px]">
-                    <div className="hidden lg:flex items-center justify-between">
-                        <div className="flex items-center gap-4">
-                            <div className="w-4 h-6 lg:h-12 bg-[#ffe347] -skew-x-[13deg]"></div>
-                            <p className="text-4xl font-extrabold uppercase">Projects</p>
-                            <div className="w-4 h-6 lg:h-12 bg-[#ffe347] -skew-x-[13deg]"></div>
-                        </div>
-                        <div className="hidden lg:flex items-center gap-4">
-                            <div className="w-4 h-6 bg-white -skew-x-[13deg]"></div>
-                            <div className="w-4 h-6 bg-white -skew-x-[13deg]"></div>
-                            <div className="w-16 h-6 bg-white -skew-x-[13deg]"></div>
-                            <div className="w-4 h-6 bg-black -skew-x-[13deg]"></div>
-                            <div className="w-16 h-6 bg-white -skew-x-[13deg]"></div>
-                            <div className="w-4 h-6 bg-[#ffe347] -skew-x-[13deg]"></div>
-                            <div className="w-4 h-6 bg-white -skew-x-[13deg]"></div>
-                            <div className="w-16 h-6 bg-white -skew-x-[13deg]"></div>
-                        </div>
-                    </div>
-                    <div className="flex flex-col gap-12 lg:gap-16 lg:mt-16 text-sm sm:text-base lg:text-2xl">
-                        <NewsletterSignup
-                            title="8x800 Athletics"
-                            description="We are all athletes. Lace up and run toward better health with the"
-                            formId="0c29f2e34c"
-                            linkText="8x800 newsletter"
-                        />
-                        <NewsletterSignup
-                            title="Fairweather Series"
-                            description="The next must-read fantasy adventure. Take flight with the"
-                            formId="0dba5570c8"
-                            linkText="Fairweather newsletter"
-                        />
-                      </div>
+                <div className="flex justify-center items-center text-3xl font-extrabold uppercase mt-24 lg-mt-48">
+                  Coming soon...
                 </div>
             </main>
             <footer className="mt-12 lg:mt-32 mb-4 lg:mb-16">
